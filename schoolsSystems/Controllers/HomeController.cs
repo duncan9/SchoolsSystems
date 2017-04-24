ca1002a573c1069ad4b8895040bf90edafc93078
duncan9 <Alekseytih@tut.by>
2017-04-24 18:45:23 +0300

    projectadd

---
+++ schoolsSystems/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace schoolsSystems.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+        public ActionResult News()
+        {
+            return View();
+        }
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
